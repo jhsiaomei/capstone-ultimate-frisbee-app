fc2ee859c3b1f83c19ccc034e1068b6bd6215ab7
jhsiaomei <jw1314@gmail.com>
2016-01-03 14:02:02 -0600

    users can join groups from group#index view, established user_group

--- app/controllers/user_groups_controller.rb
+++ app/controllers/user_groups_controller.rb
@@ -1,2 +1,29 @@
 class UserGroupsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+    @user_group = UserGroup.create(
+      user_id: current_user.id,
+      group_id: params[:group_id],
+      )
+  
+    flash[:success] = "You joined this group!"
+    redirect_to "/groups"
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end

4d54a7bd8964cb541d079d16ffab2c790842058a
jhsiaomei <jw1314@gmail.com>
2016-01-03 16:10:15 -0600

    established CST for application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :set_time_zone
+
+  def set_time_zone
+    config.time_zone = 'Central Time (US & Canada)'
+  end
 
   private
 

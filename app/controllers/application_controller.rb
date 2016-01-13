c3b8d8f936330ed0ebf292856b3b469c9e297b14
jhsiaomei <jw1314@gmail.com>
2016-01-12 21:18:08 -0600

    adjusted events#edit datetimepicker form to add 6 hours for displaying datetime

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_action :set_time_zone
 
-  def set_time_zone
-    config.time_zone = 'Central Time (US & Canada)'
-  end
 
   private
 

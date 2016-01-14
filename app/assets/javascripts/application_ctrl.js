c4928890632d82ea3eb537d4932c45f59476ef04
jhsiaomei <jw1314@gmail.com>
2016-01-13 21:07:59 -0600

    new angular controller

---
+++ app/assets/javascripts/application_ctrl.js
@@ -0,0 +1,11 @@
+
+
+angular.module['app'].controller('whateverCtrl', function($scope){
+  $scope.setup = function(){
+    get all the things from your database
+    look inside the event to select the field
+    jquery selector
+  }
+  
+  
+})

331c958976db3f7a0f9849d28b81f0472e62c5df
jhsiaomei <jw1314@gmail.com>
2016-01-13 19:19:16 -0600

    updated fields views to include navigation links through buttons

--- app/controllers/fields_controller.rb
+++ app/controllers/fields_controller.rb
@@ -26,12 +26,26 @@ class FieldsController < ApplicationController
 
   def show
     @field = Field.find(params[:id])
+    @field_placeholder = "#{@field.street_number} #{@field.street_address} | #{@field.city}, #{@field.state} #{@field.zip_code}"
   end
 
   def edit
+    @field = Field.find(params[:id])
+    @field_placeholder = "#{@field.street_number} #{@field.street_address}, #{@field.city}, #{@field.state}, #{@field.zip_code}"
   end
 
   def update
+    field = Field.find(params[:id])
+    field.update(
+      name: params[:name],
+      intersection: params[:intersection],
+      street_number: params[:street_number],
+      street_address: params[:route],
+      city: params[:locality],
+      state: params[:administrative_area_level_1],
+      zip_code: params[:postal_code]
+      )
+    redirect_to "/fields/#{field.id}"
   end
 
   def destroy
